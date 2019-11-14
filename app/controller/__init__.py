695ea8faee4745bc00e6e771b94858c661a041c9
GUTINHLIAO <gutingliao@163.com>
2019-11-14 23:02:14 +0800

    引入中间件

--- app/controller/__init__.py
+++ app/controller/__init__.py
@@ -2,9 +2,11 @@ from flask import Flask
 from app.controller.demo import demo
 from app.controller.start import start
 from app.controller.user import user
+from app.middleware.user_check import jwt_middleware
 
 
 def register_routes(app: Flask):
     app.register_blueprint(demo, url_prefix='/demo')
     app.register_blueprint(start, url_prefix='/start')
+    jwt_middleware(user)
     app.register_blueprint(user, url_prefix='/user')
