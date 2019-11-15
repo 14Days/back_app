e8afe89e5ae13dfa75d64f04bab58fa01b257111
GUTINHLIAO <gutingliao@163.com>
2019-11-15 10:52:51 +0800

    新增提交颜色接口

--- app/controller/user.py
+++ app/controller/user.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, g
 from app.model.color import get_color_list
 from app.util.response import fail_res, success_res
+from app.model.user_color import post_color
 
 user = Blueprint('user', __name__)
 
@@ -9,4 +10,10 @@ user = Blueprint('user', __name__)
 def color():
     if request.method == 'GET':
         return success_res(get_color_list())
+    if request.method == 'POST':
+        data = request.json
+        user_id = g.user_id
+        color_id = data.get('color_id')
+        post_color(user_id, color_id)
+        return success_res('提交成功')
 
--- app/model/user_color.py
+++ app/model/user_color.py
@@ -1,3 +1,15 @@
+from flask import current_app
 from app.model.define import AppUserColor
+from app.model import db
 
 
+def post_color(user_id: int, color_id: int):
+    item = AppUserColor(app_user_id=user_id, color_id=color_id)
+    res = AppUserColor.query.filter_by(app_user_id=user_id).first()
+    if res is None:
+        current_app.logger.info('新用户选择颜色')
+    else:
+        db.session.delete(res)
+        db.session.commit()
+    db.session.add(item)
+    db.session.commit()
