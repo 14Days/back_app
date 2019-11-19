ed02e7d233834f87312244f496524c370723f96b
GUTINHLIAO <gutingliao@163.com>
2019-11-19 16:20:38 +0800

    新增请求用户推荐消息接口

--- app/controller/assets.py
+++ app/controller/assets.py
@@ -2,9 +2,10 @@ from flask import Blueprint, g, current_app
 from app.model.user_color import get_color
 from app.model.color_find_tag import find_tag
 from app.util.response import success_res, fail_res
-from app.util.exception import DataBaseException
+from app.util.exception import DataBaseException, CommonException
 from app.model.tag_find_recommend import find_recommend
-from app.model.recommend import get_recommends
+from app.model.recommend import get_recommends, web_user_recommend
+from app.model.user_follow_user import get_follow
 
 assets = Blueprint('assets', __name__)
 
@@ -22,3 +23,13 @@ def recommend():
         return fail_res(e.err_msg)
 
 
+@assets.route('/follow')
+def follow():
+    try:
+        user_id = g.user_id
+        web_user_id = get_follow(user_id)
+        recommends_id = web_user_recommend(web_user_id)
+        return success_res(get_recommends(user_id, recommends_id))
+    except CommonException as e:
+        current_app.logger.error(e.err_msg)
+        return fail_res(e.err_msg)
