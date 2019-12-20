aa893257015f6d7f6778489d86bc5d22f36e7a54
GUTINHLIAO <gutingliao@163.com>
2019-12-20 11:08:52 +0800

    添加颜色id不存在时的异常处理

--- app/model/color_find_tag.py
+++ app/model/color_find_tag.py
@@ -1,8 +1,11 @@
 from app.model.define import Color
+from app.util.exception import CommonException
 
 
 def find_tag(color_id: int) -> list:
     color = Color.query.filter_by(id=color_id).first()
+    if color is None:
+        raise CommonException("用户尚未选择颜色")
     li = []
     for temp in color.second_tag:
         li.append(temp.id)
